f250cd9838274f0d3bb5937f97fd54cbaa34020a
ryan <byzero@qq.com>
2023-08-17 18:03:32 +0800

    Update case.blade.php

--- resources/views/case.blade.php
+++ resources/views/case.blade.php
@@ -24,8 +24,8 @@
         <div class="example_wrap">
             <div class="example_wrap_contain">
                 <div class="effect_contain-content">
-                    <p class="title  "></p>
-                    <p class="des  ">We focus on Investing in IT and associated Industry Transformation. Our investment areas include semiconductors, digitization, robotics, and digital energy. We are committed to helping industry-leading enterprises through value investment and empowerment. We aim to contribute to the rise of the new generation of technology companies.</p>
+                    <p class="title">PORTFOLIO SPOTLIGHT</p>
+                    <p class="des">We focus on Investing in IT and associated Industry Transformation. Our investment areas include semiconductors, digitization, robotics, and digital energy. We are committed to helping industry-leading enterprises through value investment and empowerment. We aim to contribute to the rise of the new generation of technology companies.</p>
                 </div>
             </div>
         </div>
