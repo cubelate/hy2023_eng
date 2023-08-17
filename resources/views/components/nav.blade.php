1294f0a680bf4f48dc7832ae61230236d7cd1806
ryan <byzero@qq.com>
2023-08-17 17:55:45 +0800

    Update nav.blade.php

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -5,7 +5,7 @@
     <li class="{{ \App\Util\NavUtil::getItemClass('/team.html') }}"><a href="team.html">TEAM</a></li>
     <li class="{{ \App\Util\NavUtil::getItemClass('/empower.html') }}"><a href="empower.html">GROWTH</a></li>
     <li class="{{ \App\Util\NavUtil::getItemClass('/about.html') }}"><a href="about.html">ABOUT US</a></li>
-    <li class="{{ \App\Util\NavUtil::getItemClass('/contact.html') }}"><a href="contact.html">CONTACT</a></li>
+    <li class="{{ \App\Util\NavUtil::getItemClass('/contact.html') }}"><a href="contact.html">CONTACT US</a></li>
     <li class="nav_item en"><img src="img/en.svg" alt=""></li>
 </ul>
 <!-- mobile -->
