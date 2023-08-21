0415321184096a938cff880851ea462d5c9e4e9b
ryan <byzero@qq.com>
2023-08-21 11:39:20 +0800

    update

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -6,7 +6,7 @@
     <li class="{{ \App\Util\NavUtil::getItemClass('/empower.html') }}"><a href="empower.html">GROWTH</a></li>
     <li class="{{ \App\Util\NavUtil::getItemClass('/about.html') }}"><a href="about.html">ABOUT US</a></li>
     <li class="{{ \App\Util\NavUtil::getItemClass('/contact.html') }}"><a href="contact.html">CONTACT US</a></li>
-    <li class="nav_item en"><img src="img/en.svg" alt=""></li>
+    <li class="nav_item en"><a href="https://huacapital.com/"><img src="img/cn.svg" alt=""></a></li>
 </ul>
 <!-- mobile -->
 <span class="btn_menu"></span>
