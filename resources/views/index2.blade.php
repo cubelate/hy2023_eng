e62db5afb60be4648f8152864d28e9333dcae1a5
ryan <byzero@qq.com>
2025-04-23 22:21:51 +0800

    update
    update

---
+++ resources/views/index2.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>HUA CAPITAL - HOME</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="renderer" content="webkit">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0, maximum-scale=1, user-scalable=no" />
+    <meta name="keywords" content="{{ \App\Util\MainHelper::getWebSiteDescription() }}" />
+    <meta name="description" content="{{ \App\Util\MainHelper::getWebSitekeywords() }}" />
+    <meta name="format-detection" content="telephone=no" />
+
+</head>
+<body>
+<div style="padding: 100px">
+    <img style="margin: auto" src="./img/image.png" />
+</div>
+</body>
+</html>
